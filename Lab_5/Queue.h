ddfff539ae737d8fd521a1f8628872cb83f1618c
Tim Elvart <telvart@ku.edu>
2017-02-21 12:04:37 -0600

    Work on lab5

--- Lab_5/Queue.h
+++ Lab_5/Queue.h
@@ -2,13 +2,24 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "Node.h"
+
 template<typename T>
 class Queue
 {
   public:
     Queue();
-
+    ~Queue();
+    void enqueue(T val);
+    Node<T>* dequeue(T val);
+    bool isEmpty();
+    void size();
   private:
+
+    int m_size;
+    Node<T>* m_front;
+    Node<T>* m_rear;
+
 };
 
 #include "Queue.hpp"
--- Lab_5/Queue.hpp
+++ Lab_5/Queue.hpp
@@ -2,5 +2,52 @@
 template <typename T>
 Queue<T>::Queue()
 {
+  m_front=nullptr;
+  m_rear=nullptr;
+  m_size=0;
+}
 
+template <typename T>
+Queue<T>::~Queue()
+{
+  while(!isEmpty())
+  {
+    dequeue(m_front->getData());
+  }
+}
+
+template <typename T>
+void Queue<T>::enqueue(T val)
+{
+  Node<T>* temp = new Node<T>(val);
+  if(isEmpty())
+  {
+    m_front = temp;
+    m_rear = temp;
+  }
+  else
+  {
+    m_rear->setNext(temp);
+    temp->setPrev(m_rear);
+    m_rear = temp;
+  }
+  m_size++;
+}
+
+template <typename T>
+Node<T>* Queue<T>::dequeue(T val)
+{
+  return new Node<T>(val);
+}
+
+template <typename T>
+bool Queue<T>::isEmpty()
+{
+  return m_front == nullptr;
+}
+
+template <typename T>
+void Queue<T>::size()
+{
+  return m_size;
 }
